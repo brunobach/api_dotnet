bc5c91e38a44c08e7afd71a689006ced2a2fb535
Bruno Bach <brunobach@iMac-Pro.local>
2020-07-13 20:05:47 -0300

    Add archives init

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProjectSchool_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            return Ok(new {
+                id = 1,
+                nome = "Bruno Bach",
+                sobrenome = "de Azevedo"
+            });
+        }
+    }
+}
